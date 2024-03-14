496b7f698dc45d637e0764e09c9b90bafadf54be
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-13 21:42:09 -0300

    feat: Criando header

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsam, aut ducimus molestiae consectetur adipisci assumenda vitae maxime doloribus praesentium eligendi animi similique totam sint tempora fugiat, placeat voluptates maiores."
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsam, aut ducimus molestiae consectetur adipisci assumenda vitae maxime doloribus praesentium eligendi animi similique totam sint tempora fugiat, placeat voluptates maiores."
+      />
+    </div>
+  );
+}

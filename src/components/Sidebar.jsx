62916ce2155e1fedd55051443047dcdc375fea5f
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-14 21:31:45 -0300

    feat: Mudando a imagem de capa da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://plus.unsplash.com/premium_photo-1683309565422-77818a287060?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
         <strong>Victor Gadelha</strong>

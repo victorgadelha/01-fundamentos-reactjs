d6a8e99e1827d9a4eb88bc42141739351b84ff9b
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-14 21:29:07 -0300

    feat: Criando e estilizando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { Header } from "./components/Header";
 import styles from "./App.module.css";
 
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <aside>sidebar</aside>
+        <Sidebar />
         <main>
           <Post
             author="Diego Fernandes"

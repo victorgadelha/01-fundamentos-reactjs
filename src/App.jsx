757724c44fee8508c52144b6b686f44de011111f
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-14 21:14:02 -0300

    feat: Criando wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
+
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsam, aut ducimus molestiae consectetur adipisci assumenda vitae maxime doloribus praesentium eligendi animi similique totam sint tempora fugiat, placeat voluptates maiores."
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsam, aut ducimus molestiae consectetur adipisci assumenda vitae maxime doloribus praesentium eligendi animi similique totam sint tempora fugiat, placeat voluptates maiores."
-      />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsam, aut ducimus molestiae consectetur adipisci assumenda vitae maxime doloribus praesentium eligendi animi similique totam sint tempora fugiat, placeat voluptates maiores."
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsam, aut ducimus molestiae consectetur adipisci assumenda vitae maxime doloribus praesentium eligendi animi similique totam sint tempora fugiat, placeat voluptates maiores."
+          />
+        </main>
+      </div>
     </div>
   );
 }

63e895a7622490b1328bc2d88abfe5d6a30ed522
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-16 19:57:58 -0300

    feat: Adicionando foto de perfil na sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,10 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/victorgadelha.png"
+        />
         <strong>Victor Gadelha</strong>
         <span>Web Developer</span>
       </div>
